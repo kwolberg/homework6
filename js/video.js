a9d9e450da5a742cc2da81b894bfdbbe1734bf48
kwolberg <kwolberg@umich.edu>
2020-11-18 17:03:19 -0500

    homework 6 work

--- js/video.js
+++ js/video.js
@@ -1,14 +1,73 @@
-var video;
+var video = document.getElementById("myVideo");
 
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+	video.play()
+	console.log("Play video");
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause()
+	console.log("Pause video");
+
 });
+
+document.querySelector("#slower").addEventListener("click", function() {
+	var currentSpeed = video.playbackRate;
+	video.playbackRate = .9 * (currentSpeed);
+	console.log("Current speed: " + currentSpeed);
+});
+
+
+document.querySelector("#faster").addEventListener("click", function() {
+	var currentSpeed = video.playbackRate;
+	video.playbackRate = (currentSpeed) / .9;
+	console.log("Current speed: " + currentSpeed);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 5 > video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 5;
+	}
+	console.log("Skipped");
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	var muted = video.muted; 
+	if (video.muted == true) {
+		video.muted = false; 
+		console.log("Video Unmuted");
+		this.innerHTML = "Mute";
+	}
+	else {
+		video.muted = true; 
+		console.log("Video Muted");
+		this.innerHTML = "Unmute";
+	}
+});
+
+
+document.querySelector("#volumeSlider").addEventListener("change", function(event) {
+	video.volume = event.currentTarget.value / 100;
+	console.log("Volume is: " + video.volume);
+	document.querySelector('#volume').innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.style.filter = "grayscale(100%)"; 
+	console.log("Video changed to old school");
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.style.filter = "grayscale(0%)"; 
+	console.log("Video changed to original");
+});
+
+
 
